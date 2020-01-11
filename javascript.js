3cfedb7c419a4ff6170d872d7ce5fd2df4e10a5b
kryvousnazar <nazar_-@ukr.net>
2020-01-12 01:42:51 +0200

    second commit

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+		$(".button__location__logo").click(function(){
+				$('.contacts__button__location').toggleClass("show");
+				$('.work__text__location').toggleClass("work__text__location");		
+			});
+			
+
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next')
+});
+
+
+$('.slider__teem').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3
+  });
+
+  function myMap() {
+	var mapProp= {
+	  center:new google.maps.LatLng(49.553520,25.594770),
+	  zoom:8
+	};
+
+	var myMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(49.553520, 25.594770),
+		draggable: true
+	});
+	
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	}
+	myMarker.googleMap(map);
+
+
+
